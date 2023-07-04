e0f2fd9912f8d56366f582717d258da7ff08f211
adisusioh31 <137909770+adisusioh31@users.noreply.github.com>
2023-07-04 18:00:08 +0900

    Update Patches.cs
    Fixed uploading wrong source code

--- SprintOn/Patches.cs
+++ SprintOn/Patches.cs
@@ -1,163 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading;
-using GYKHelper;
+using BepInEx.Logging;
 using HarmonyLib;
-using SaveNow.lang;
+using LazyBearGames.Preloader;
+using Rewired;
+using System.Reflection;
+using System;
 using UnityEngine;
 
-namespace SaveNow;
-
-[HarmonyPatch]
-public partial class Plugin
+namespace SprintOn
 {
-    private static Vector3 Pos { get; set; }
-    private static string DataPath { get; set; }
-    private static string SavePath { get; set; }
-    private static readonly List<SaveSlotData> AllSaveGames = new();
-    private static List<SaveSlotData> SortedTrimmedSaveGames  { get; set; } = new();
-    private static bool CanSave { get; set; }
-    private static string CurrentSave { get; set; }
-    private static readonly Dictionary<string, Vector3> SaveLocationsDictionary = new();
-
-
-    [HarmonyPrefix]
-    [HarmonyPatch(typeof(SaveSlotsMenuGUI), nameof(SaveSlotsMenuGUI.RedrawSlots))]
-    public static void SaveSlotsMenuGUI_RedrawSlots(ref List<SaveSlotData> slot_datas, ref bool focus_on_first)
+    [HarmonyPatch]
+    public static class Patches
     {
-        slot_datas.Clear();
-        AllSaveGames.Clear();
-        SortedTrimmedSaveGames.Clear();
+        internal static bool _sprintingIsToggled { get; set; }
+        internal static bool _IsKeyPressed { get; set; }
+        internal static bool _IsPadButtonPressed { get; set; }
 
-        LoadSaveGames();
-
-        SortedTrimmedSaveGames = SortSaveGames();
-
-        if (SortedTrimmedSaveGames.Count > MaximumSavesVisible.Value)
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(MovementComponent), nameof(MovementComponent.UpdateMovement))]
+        public static void MovementComponentUpdatePatch(MovementComponent __instance)
         {
-            Resize(SortedTrimmedSaveGames, MaximumSavesVisible.Value);
-        }
-
-        slot_datas = SortedTrimmedSaveGames;
-        focus_on_first = true;
-    }
-
-    private static void LoadSaveGames()
-    {
-        var saveFiles = Directory.GetFiles(PlatformSpecific.GetSaveFolder(), "*.info",
-            SearchOption.TopDirectoryOnly);
-
-        foreach (var text in saveFiles)
-        {
-            var data = SaveSlotData.FromJSON(File.ReadAllText(text));
-            if (data == null) continue;
-            data.filename_no_extension = Path.GetFileNameWithoutExtension(text);
-            AllSaveGames.Add(data);
-        }
-    }
-
-    private static List<SaveSlotData> SortSaveGames()
-    {
-        return SortByRealTime.Value
-            ? (AscendingSort.Value
-                ? AllSaveGames.OrderBy(o => o.real_time).ToList()
-                : AllSaveGames.OrderByDescending(o => o.real_time).ToList())
-            : (AscendingSort.Value
-                ? AllSaveGames.OrderBy(o => o.game_time).ToList()
-                : AllSaveGames.OrderByDescending(o => o.game_time).ToList());
-    }
-
-
-    [HarmonyPrefix]
-    [HarmonyPatch(typeof(InGameMenuGUI), nameof(InGameMenuGUI.OnPressedSaveAndExit))]
-    public static bool InGameMenuGUI_OnPressedSaveAndExit(InGameMenuGUI __instance)
-    {
-        if (!Tools.TutorialDone()) return true;
-        Thread.CurrentThread.CurrentUICulture = CrossModFields.Culture;
-
-        __instance.SetControllsActive(false);
-        __instance.OnClosePressed();
-
-        var messageText = CreateMessageText();
-
-        var dialog = GUIElements.me.dialog;
-        dialog.OpenYesNo(messageText, SaveAndExit);
-
-        return false;
-
-        string CreateMessageText()
-        {
-            var baseMessage = ExitToDesktop.Value ? strings.SaveAreYouSureDesktop : strings.SaveAreYouSureMenu;
-            var progressMessage = DisableSaveOnExit.Value || CrossModFields.IsInDungeon ? strings.SaveProgressNotSaved : strings.SaveProgressSaved;
-
-            return $"{baseMessage}?\n\n{progressMessage}.";
-        }
-
-        void SaveAndExit()
-        {
-            if (DisableSaveOnExit.Value || CrossModFields.IsInDungeon)
+            if (__instance.wgo.is_player)
             {
-                PerformExit();
-            }
-            else
-            {
-                if (SaveLocation(true, string.Empty))
+                if (_IsKeyPressed == true || _IsPadButtonPressed == true)
                 {
-                    PlatformSpecific.SaveGame(MainGame.me.save_slot, MainGame.me.save, delegate { PerformExit(); });
+                    Patches.SetSpeed(__instance, _sprintingIsToggled ? LazyConsts.PLAYER_SPEED : Plugin.SprintSpeed.Value);
+                    _sprintingIsToggled = _sprintingIsToggled != true;
+                    _IsKeyPressed = false;
+                    _IsPadButtonPressed = false;
+                }
+                if (_sprintingIsToggled && !Patches.DrainEnergy(__instance))
+                {
+                    Patches.SetSpeed(__instance, LazyConsts.PLAYER_SPEED);
+                    _sprintingIsToggled = false;
+                    return;
                 }
             }
         }
 
-        void PerformExit()
+        private static bool DrainEnergy(MovementComponent instance)
         {
-            if (ExitToDesktop.Value)
+            if ((double)Math.Abs(LazyInput.GetDirection().x) > 0.2 || (double)Math.Abs(LazyInput.GetDirection().y) > 0.2)
             {
-                GC.Collect();
-                Resources.UnloadUnusedAssets();
-                Application.Quit();
+                return instance.wgo.components.character.player.TrySpendEnergy(Plugin.EnergyConsumption.Value);
             }
-            else
+            return MainGame.me.player.energy >= Plugin.EnergyConsumption.Value;
+        }
+
+        private static void SetSpeed(MovementComponent instance, float targetSpeed)
+        {
+            if (instance.wgo.data.GetParam("speed", 0f) != targetSpeed)
             {
-                LoadingGUI.Show(__instance.ReturnToMainMenu);
+                instance.SetSpeed(targetSpeed);
             }
         }
-    }
-
-
-    // if this isn't here, when you sleep, it teleport you back to where the mod saved you last
-    [HarmonyPrefix]
-    [HarmonyPatch(typeof(SleepGUI), nameof(SleepGUI.WakeUp))]
-    public static void SleepGUI_WakeUp()
-    {
-        SaveLocation(false, string.Empty);
-    }
-
-
-    [HarmonyPostfix]
-    [HarmonyPatch(typeof(InGameMenuGUI), nameof(InGameMenuGUI.Open))]
-    public static void InGameMenuGUI_Open(ref InGameMenuGUI __instance)
-    {
-        if (__instance == null || !ExitToDesktop.Value) return;
-
-        var exitButtons = __instance.GetComponentsInChildren<UIButton>()
-            .Where(x => x.name.Contains("exit"));
-
-        foreach (var comp in exitButtons)
-        {
-            var exitLabels = comp.GetComponentsInChildren<UILabel>();
-            foreach (var label in exitLabels)
-            {
-                label.text = strings.ExitButtonText;
-            }
-        }
-    }
-    
-    [HarmonyPostfix]
-    [HarmonyPatch(typeof(MovementComponent), nameof(MovementComponent.UpdateMovement), null)]
-    public static void MovementComponent_UpdateMovement(MovementComponent __instance)
-    {
-        CanSave = !__instance.player_controlled_by_script;
     }
 }
